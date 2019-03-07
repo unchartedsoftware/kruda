dd165914bd1ebeddf6d4469fde6fdd888e54a9b7
Dario Segura <dsegura@uncharted.software>
2019-03-07 13:51:17 -0500

    [DOCS] Row documentation.

--- src/data/Row.js
+++ src/data/Row.js
@@ -24,7 +24,20 @@ import {Pointer} from '../core/Pointer';
 import * as Types from '../core/Types';
 import {ByteString} from './ByteString';
 
+/**
+ * Class to read and write values of a row in a Table.
+ * @class Row
+ */
 export class Row {
+    /**
+     * Constructs an instance of a row in the given table at the specified index.
+     * Each Row instance automatically adds new properties with the names of the columns to its `fields` object for
+     * easy access.
+     * WARNING: String returned by a row will mutate when the row's address changes, if strings with constant values are
+     * needed, either copy of the string or create a JS string from it by calling `toString` on it.
+     * @param {Table} table - The table this row belongs to.
+     * @param {number=} index - the row index at which this instance will read data. Defaults to 0.
+     */
     constructor(table, index = 0) {
         this.mTable = table;
         this.mTableOffset = this.mTable.mDataOffset;
@@ -32,52 +45,104 @@ export class Row {
         this.mIndex = index;
         this.mPointer = new Pointer(this.mTable.memory, this.mTableOffset + this.mIndex * this.mSize, Types.Void);
         this.mAccessors = {};
+        this.mFields = {};
 
         this.mTable.header.order.forEach(column => {
             const description = this.mTable.header.columns[column];
             const accessor = {
                 column,
                 getter: this._createPropertyGetter(description, this.mPointer),
-                setter: null,
+                setter: () => {}, // not implemented yet
             };
 
             this.mAccessors[column] = accessor;
 
-            Object.defineProperty(this, column, {
-                get: () => accessor.getter(),
+            Object.defineProperty(this.mFields, column, {
+                get: accessor.getter,
+                set: accessor.setter,
             });
         });
     }
 
+    /**
+     * The size, in bytes, of a row in the table.
+     * @return {number}
+     */
     get size() {
         return this.mSize;
     }
 
+    /**
+     * The table this row belongs to.
+     * @return {Table}
+     */
     get table() {
         return this.mTable;
     }
 
+    /**
+     * An array containing the names of the columns in the table this row belongs to.
+     * @return {Array}
+     */
     get columns() {
         return this.mTable.header.orderOriginal;
     }
 
+    /**
+     * An object containing accessor objects ({{column:string, getter:function():*, setter:null}}) for the fields in
+     * this row.
+     * @return {object}
+     */
     get accessors() {
         return this.mAccessors;
     }
 
+    /**
+     * An object containing properties to get and set the values for the fields in this row based on their column names.
+     * NOTE: Setting values is not implemented yet.
+     * @return {object}
+     */
+    get fields() {
+        return this.mFields;
+    }
+
+    /**
+     * The internal pointer this row uses to access its memory. Changing the location of this pointer will result in
+     * the contents of the row being updated.
+     * WARNING: Setting the address of this pointer to a memory address taht does not represent the beginning of a
+     * row in a table will result in undefined behaviour.
+     * @return {Pointer}
+     */
     get pointer() {
         return this.mPointer;
     }
 
+    /**
+     * The row index this instance is currently pointing at.
+     * @return {number}
+     */
     get index() {
         return this.mIndex;
     }
 
+    /**
+     * Sets the row index this row should be pointing at. Does not take into consideration the internal pointer address
+     * to calculate the new row address so it's safe to use to reset the internal pointer address.
+     * @param {number} value - The new index.
+     */
     set index(value) {
         this.mIndex = value;
         this.mPointer.address = this.mTableOffset + this.mIndex * this.mSize;
     }
 
+    /**
+     * Creates a function that returns the contents of a column's field as specified by the `description` object.
+     * NOTE: The returned functions make use of the row's internal pointer for efficiency.
+     * @param {{type:string, size:number, offset:number}} description - Descriptions of the column this field belongs to.
+     * @param {Pointer} pointer - The row's internal pointer.
+     * @return {function():*}
+     * @private
+     */
     _createPropertyGetter(description, pointer) {
         const offset = description.offset;
         const type = Types.typeByName(description.type);
