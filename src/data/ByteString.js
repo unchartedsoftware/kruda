f8ec6b8b04627b3645a731eb9a5af90182d03676
Dario Segura <dsegura@uncharted.software>
2019-03-07 13:22:57 -0500

    [DOCS] ByteString documentation.

--- src/data/ByteString.js
+++ src/data/ByteString.js
@@ -23,10 +23,28 @@
 import {Type} from '../core/Types';
 import {Pointer} from '../core/Pointer';
 
+/**
+ * Constant variable, represents the character code of the uppercase letter `A`, used to convert string to lower case.
+ * @type {number}
+ */
 const kCodeA = ('A').charCodeAt(0);
+
+/**
+ * Constant variable, represents the character code of the uppercase letter `Z`, used to convert string to lower case.
+ * @type {number}
+ */
 const kCodeZ = ('Z').charCodeAt(0);
 
+/**
+ * Base class for all byte string classes. Cannot be used directly.
+ * @class ByteString
+ * @private
+ */
 class ByteStringBase {
+    /**
+     * Constructs a ByteString instance of the given size.
+     * @param {number} size - The maximum size of this string.
+     */
     constructor(size = 256) {
         /// #if DEBUG
         if (size > 256) {
@@ -38,6 +56,13 @@ class ByteStringBase {
         /// #endif
 
         this.mSize = size;
+    }
+
+    /**
+     * @type {number}
+     */
+    get size() {
+        return this.mSize;
     }
 
     /**
@@ -61,11 +86,20 @@ class ByteStringBase {
         throw 'Not implemented';
     }
 
+    /**
+     * Utility function, converts this ByteString to a JS string instance.
+     * @return {string}
+     */
     toString() {
         const arr = new Uint8Array(this.buffer, this.address + 1, this.length);
         return String.fromCharCode(...arr);
     }
 
+    /**
+     * Checks if two byte strings are equal. Case sensitive.
+     * @param {ByteStringBase} other - The string to test against.
+     * @return {boolean}
+     */
     equals(other) {
         if (other.length !== this.length) {
             return false;
@@ -80,6 +114,11 @@ class ByteStringBase {
         return true;
     }
 
+    /**
+     * Checks if two byte strings are equal. Case insensitive.
+     * @param {ByteStringBase} other - The string to test against.
+     * @return {boolean}
+     */
     equalsCase(other) {
         if (other.length !== this.length) {
             return false;
@@ -94,6 +133,11 @@ class ByteStringBase {
         return true;
     }
 
+    /**
+     * Checks if this byte strings contains another string. Case sensitive.
+     * @param {ByteStringBase} other - The string to test against.
+     * @return {boolean}
+     */
     contains(other) {
         const nn = other.length;
         for (let i = 0, n = 1 + this.length - nn; i < n; ++i) {
@@ -112,6 +156,11 @@ class ByteStringBase {
         return false;
     }
 
+    /**
+     * Checks if this byte strings contains another string. Case insensitive.
+     * @param {ByteStringBase} other - The string to test against.
+     * @return {boolean}
+     */
     containsCase(other) {
         const nn = other.length;
         for (let i = 0, n = 1 + this.length - nn; i < n; ++i) {
@@ -141,60 +190,121 @@ class ByteStringBase {
     }
     /* eslint-enable */
 
+    /**
+     * Converts the given character code to its lowercase code.
+     * @param {number} c - The character code to convert.
+     * @return {number}
+     * @private
+     */
     _toLower(c) {
         return (c >= kCodeA && c <= kCodeZ) ? (c + 32) : c;
     }
 }
 
+/**
+ * ByteString implementation using pointers. If the underlying pointer changes location the contents of this string
+ * are automatically updated to reflect the data contained at the new location.
+ * @class ByteStringPtr
+ * @private
+ */
 class ByteStringPtr extends ByteStringBase {
+    /**
+     * Constructs a byte string instance backed by a Pointer.
+     * @param {Pointer} pointer - The pointer to read the string data from.
+     * @param {number=} offset - The offset, from the location of the pointer, to read the data from. Defaults to 0.
+     * @param {number=} size - The maximum size of this string in bytes. Defaults to 256.
+     */
     constructor(pointer, offset = 0, size = 256) {
         super(size);
         this.mOffset = offset;
         this.mPointer = pointer;
     }
 
+    /**
+     * @type {number}
+     */
     get length() {
         return this.mPointer.getUint8(this.mOffset);
     }
 
+    /**
+     * @type {ArrayBufferLike}
+     */
     get buffer() {
         return this.mPointer.memory.buffer;
     }
 
+    /**
+     * @type {number}
+     */
     get address() {
         return this.mPointer.memory.address + this.mPointer.address + this.mOffset;
     }
 
+    /**
+     * Fetches the character code at the specified index.
+     * @param {number} index - The index of the character to fetch.
+     * @return {number}
+     */
     charCodeAt(index) {
         return this.mPointer.getUint8(this.mOffset + index + 1);
     }
 }
 
+/**
+ * ByteString implementation using ArrayBuffers. This implementation is useful for quick off-heap strings. It also
+ * guarantees that it will no change its contents without implicit user interaction.
+ * @class ByteStringBuffer
+ * @private
+ */
 class ByteStringBuffer extends ByteStringBase {
-    constructor(buffer, address, size = 256) {
+    /**
+     * Constructs a byte string backed by an ArrayBuffer.
+     * @param {ArrayBufferLike} buffer - The ArrayBuffer object where this string resides.
+     * @param {number=} address - The offset, in bytes, within the buffer where the string resides. Defaults to 0.
+     * @param {number=} size - The maximum size of this string in bytes. Defaults to 256.
+     */
+    constructor(buffer, address = 0, size = 256) {
         super(size);
         this.mBuffer = buffer;
         this.mAddress = address;
         this.mView = new DataView(buffer, this.mAddress, this.mSize);
     }
 
+    /**
+     * @type {ArrayBufferLike}
+     */
     get buffer() {
         return this.mBuffer;
     }
 
+    /**
+     * @type {number}
+     */
     get length() {
         return this.mView.getUint8(0);
     }
 
+    /**
+     * @type {number}
+     */
     get address() {
         return this.mAddress;
     }
 
+    /**
+     * Fetches the character code at the specified index.
+     * @param {number} index - The index of the character to fetch.
+     * @return {number}
+     */
     charCodeAt(index) {
         return this.mView.getUint8(index + 1);
     }
 }
 
+/**
+ * ByteString type
+ */
 class _ByteString extends Type {
     constructor() {
         super('ByteString', 256, 2048);
@@ -216,21 +326,50 @@ class _ByteString extends Type {
         }
     }
 
-    new(arg1, arg2, size = 256) {
+    /**
+     * Creates a new ByteString instance based on the provided arguments. Using the more specific `fromPointer`,
+     * `fromBuffer` and `fromString` functions is recommended.
+     * @param {Pointer|ArrayBufferLike|String} arg1 - The memory object from which the string will be copied from.
+     * @param {number=} arg2 - An offset, if needed for the resulting instance. Defaults to 0.
+     * @param {number} size - The maximum size, if needed, of the resulting instance. Defaults to 256.
+     * @return {ByteStringBase}
+     */
+    new(arg1, arg2 = 0, size = 256) {
         if (arg1 instanceof Pointer) {
-            return new ByteStringPtr(arg1, arg2, size);
+            return this.fromPointer(arg1, arg2, size);
+        } else if (typeof arg1 === 'string' || arg1 instanceof String) {
+            return this.fromString(arg1);
         }
-        return new ByteStringBuffer(arg1, arg2, size);
+        return this.fromBuffer(arg1, arg2, size);
     }
 
+    /**
+     * Creates a ByteString instance from a pointer.
+     * @param {Pointer} pointer - The pointer to read the string data from.
+     * @param {number=} offset - The offset, from the location of the pointer, to read the data from. Defaults to 0.
+     * @param {number=} size - The maximum size of this string in bytes. Defaults to 256.
+     * @return {ByteStringPtr}
+     */
     fromPointer(pointer, offset = 0, size = 256) {
         return new ByteStringPtr(pointer, offset, size);
     }
 
-    fromBuffer(buffer, address, size = 256) {
+    /**
+     * Creates a ByteString instance from an ArrayBuffer
+     * @param {ArrayBufferLike} buffer - The ArrayBuffer object where this string resides.
+     * @param {number=} address - The offset, in bytes, within the buffer where the string resides. Defaults to 0.
+     * @param {number=} size - The maximum size of this string in bytes. Defaults to 256.
+     * @return {ByteStringBuffer}
+     */
+    fromBuffer(buffer, address = 0, size = 256) {
         return new ByteStringBuffer(buffer, address, size);
     }
 
+    /**
+     * Creates a byte string from a JS string.
+     * @param {string} str - The string to copy during creation.
+     * @return {ByteStringBuffer}
+     */
     fromString(str) {
         const length = Math.min(str.length, 255);
         const view = new Uint8Array(length + 1);
