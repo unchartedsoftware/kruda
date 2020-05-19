2519ca925338b942ab7c37a52cab6eddf13639dc
darionco <darionco.dev@gmail.com>
2020-05-19 00:14:16 -0400

    [HOUSEKEEPING] Removed unused import

--- src/data/filter/Filter.js
+++ src/data/filter/Filter.js
@@ -20,7 +20,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-import {Pointer} from '../../core/Pointer';
 import * as Types from '../../core/Types';
 import FilterWorker from 'web-worker:./Filter.worker';
 import {FilterWorkerDummy} from './Filter.worker.dummy';
